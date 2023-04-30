8a5f260dafe0d06d9ecb57f8957d6e49b418b00d
JanverNike <jconnor@mauvesolutions.com>
2023-04-30 12:59:23 -0600

    Update for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://janvernike.github.io",
+  base: '/mauvesolutions',
   integrations: [
     tailwind(),
     image({
